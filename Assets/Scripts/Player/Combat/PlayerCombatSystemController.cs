f9213a349c00101cb9910035cb8ad62b3c16c034
glebianK <glebian001@gmail.com>
2025-01-26 21:52:50 +0300

    Created script PlayerCombatSystemController
    - Создал скрипт комбат контроллера
    - Создал папки под скрипты атак и блока

---
+++ Assets/Scripts/Player/Combat/PlayerCombatSystemController.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerCombatSystemController : MonoBehaviour
+{
+    private float attackPreparationTimer;
+    private bool timerIsCounting;
+
+    public bool CanAttack { get; set; }
+
+    private void Start()
+    {
+        CanAttack = true;
+        attackPreparationTimer = 0f;
+        timerIsCounting = false;
+    }
+
+    private IEnumerator AttackPreparationTimer()
+    {
+        Debug.LogWarning("AttackPreparationTimer coroutine started!");
+        if (CanAttack)
+        {
+            attackPreparationTimer = 0f;
+            timerIsCounting = true;
+
+            while (timerIsCounting)
+            {
+                attackPreparationTimer += Time.deltaTime;
+                Debug.Log($"TIMER! value = {attackPreparationTimer}");
+                yield return null;
+            }
+            Debug.Log($"timer ends, value = {attackPreparationTimer}");
+        }
+        Debug.LogWarning("End of coroutine");
+        yield return null;
+    }
+
+    #region INPUT CALLBACKS
+    public void OnAttackPrepare(InputAction.CallbackContext context)
+    {
+
+        CanAttack = true;
+
+        if (context.performed)
+        {
+            Debug.LogWarning("Ready to attack!!!");
+            StartCoroutine(AttackPreparationTimer());
+            //TODO: Invoke attack preparation event (run the animation?)
+        }
+    }
+
+    public void OnAttackInitiate(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.LogWarning("ATTACK !!!");
+            CanAttack = false;
+            timerIsCounting = false;
+            //TODO: Ivoke attack event from attack system (run the animation?)
+        }
+    }
+
+    public void OnBlock(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.LogWarning("Player block is not implemented !!!");
+            throw new NotImplementedException("PlayerCombatController.cs -> OnBlock - IMPLEMENT!");
+        }
+    }
+    #endregion
+}
