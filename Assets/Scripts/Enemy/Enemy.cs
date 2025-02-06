73af88f31c3c833bf9424aa167126c6536483124
cergeypp <segslipknot03@gmail.com>
2025-02-06 06:19:04 +0300

    Fix UI tip, import new LD

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Events;
-using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
 public class Enemy : MonoBehaviour
 {
