1ebefa48835c7d9e917045c7390136eadad1f36f
glebianK <glebian001@gmail.com>
2025-02-08 16:49:50 +0300

    Added Sergei enemy attck fixes into new enemy system

--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -8,17 +8,33 @@ public class EnemyBase : MonoBehaviour
     [SerializeField] protected Health health;
     [SerializeField] protected AttackEnemy attack;
     [SerializeField] protected MotionEnemy motionEnemy;
+    [SerializeField] protected float attackStartRange;
+    [SerializeField] protected float maxPlayerDetectViewAngle = 15.0f;
     [SerializeField] protected NavMeshAgent agent;
     [SerializeField] protected float speedAgent;
-    [SerializeField] protected float attackStartRange;
+    [SerializeField] protected float angularSpeedAgent;
 
     protected Transform player;
-    protected readonly float smoothnessOfRotation = 0.015f; // отвечает за плавность поворота мобов в стороны игрока; значения от 0f до 1f
+    protected readonly float smoothnessOfRotation = 0.015f; // отвечает за плавность поворота мобов в сторону игрока; значения от 0f до 1f
 
+    #region UNITY METHODS
     private void Update()
     {
         EnemyBehaviorCycle();
     }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        DetectPlayer(other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        LosePlayer(other);
+    }
+    #endregion
+
+    #region USER PROTECTED METHODS
 
     protected virtual void EnemyBehaviorCycle()
     {
@@ -29,43 +45,29 @@ public class EnemyBase : MonoBehaviour
 
             if (distance <= attackStartRange)
             {
-                AttackPlayer();
+                agent.speed = 0f;
+                if (attack.CanAttack())
+                {
+                    float angle = Vector3.Angle(transform.forward, (player.position - transform.position).normalized);
+                    if (angle < maxPlayerDetectViewAngle)
+                    {
+                        AttackPlayer();
+                    }
+                }
             }
-            else
+            else if (attack.CanAttack())
             {
                 ChasePlayer();
             }
         }
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        DetectPlayer(other);
-        /*
-        if (other.CompareTag("Player"))
-        {
-            player = other.transform;
-        }
-        */
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        LosePlayer(other);
-        /*
-        if (other.CompareTag("Player"))
-        {
-            agent.speed = 0;
-            motionEnemy.StopMovingAnimations();
-            player = null;
-        }
-        */
-    }
-
     protected Quaternion RotateInDirectionOfPlayer(float smoothness)
     {
         Vector3 directionToPlayer = player.position - transform.position;
-        Quaternion rotationToPlayer = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(directionToPlayer), smoothness);
+        //Quaternion rotationToPlayer = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(directionToPlayer), smoothness);
+        Quaternion rotationToPlayer = Quaternion.RotateTowards(transform.rotation, 
+            Quaternion.LookRotation(directionToPlayer), angularSpeedAgent * Time.deltaTime);
 
         return rotationToPlayer;
     }
@@ -73,14 +75,13 @@ public class EnemyBase : MonoBehaviour
     protected void ChasePlayer()
     {
         agent.speed = speedAgent;
+        agent.angularSpeed = angularSpeedAgent;
         motionEnemy.StartMovingAnimations();
         agent.SetDestination(player.position);
     }
 
     protected void AttackPlayer()
     {
-        agent.speed = 0;
-        motionEnemy.StopMovingAnimations();
         attack.TryAttack();
     }
 
@@ -101,9 +102,24 @@ public class EnemyBase : MonoBehaviour
             player = null;
         }
     }
+    #endregion
+
+    #region EVENT CALLBACKS
+    public void AttackStarted()
+    {
+        agent.speed = 0;
+        agent.angularSpeed = 0;
+        motionEnemy.StopMovingAnimations();
+    }
+
+    public void AttackEnded()
+    {
+        ChasePlayer();
+    }
 
     public void EnemyDied()
     {
         Destroy(gameObject);
     }
+    #endregion
 }
